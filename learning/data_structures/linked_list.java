93e474c77a885ad9366e159d62e4b27d7e1852c7
Mohammed Elali <mohammedelali@Mohammeds-MBP.lan>
2024-12-16 23:34:40 -0500

    linked list BASIC

--- learning/data_structures/linked_list.java
+++ learning/data_structures/linked_list.java
@@ -1,5 +1,18 @@
 package learning.data_structures;
 
+import java.util.LinkedList;
+
 public class linked_list {
+    
+    public static void main(String[] args) {
+        // Simple Linked List
+        LinkedList<String> luciousList = new LinkedList<String>();
+        luciousList.add("balls?");
+        luciousList.add("any questions?");
+        luciousList.add("yumulus");
+        luciousList.add("oiled up guy");
+
+        System.out.println(luciousList);
+    }
     
 }
