61b1d64ea959ed13b44fedb47712e664d0f210cc
Mohammed Elali <mohammedelali@Mohammeds-MBP.lan>
2024-12-16 20:56:35 -0500

    Bolier Plate for Data Structure Work

---
+++ data_structures/array.java
@@ -0,0 +1,28 @@
+package data_structures;
+   
+public class array {
+     //How to create an array and replace an item in that array
+    public void ChangeArray(){
+        String[] cars2 = {"Bimmer", "Yotas", "Mazdas"};
+        cars2[0] = "Utica";
+        System.out.println(cars2[0]); 
+    }
+    // How to create a simple array and prinout the entry you want
+    public void ArrayDisplay(){
+        String[] cars = {"Bimmer", "Yotas", "Mazdas"};
+        System.out.println(cars[1]);
+    }
+    // How to display size of array
+    public void ArraySize(){
+        String[] cars = {"Bimmer", "Yotas", "Mazdas"};
+        System.out.println(cars.length);
+    }
+    
+    public static void main(String[] args) {
+        array obj = new array();
+        obj.ArrayDisplay();
+        obj.ChangeArray();
+        obj.ArraySize();
+    }
+
+}
---
+++ data_structures/arrayLoops.java
@@ -0,0 +1,21 @@
+package data_structures;
+
+public class arrayLoops {
+    public static void main(String[] args) {
+        arrayLoops obj = new arrayLoops();
+        obj.loopArray();
+        obj.forEachArray();
+    }
+    public void loopArray(){
+        String[] cars = {"Bimmer", "Yotas", "Mazdas"};
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+          }
+    }
+    public void forEachArray(){
+        String[] cars = {"Bimmer", "Yotas", "Mazdas"};
+        for (String i : cars){
+            System.out.println(i);
+        }           
+    }
+}
